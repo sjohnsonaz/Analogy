0478dbe4ec8b4e24b7d62f0ca6e98330253ccaa5
Sean Johnson <sjohnson@sjohnsonaz.com>
2015-12-18 17:15:34 -0700

    Fixed gulpfile and distribution.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,19 @@
 var gulp = require('gulp');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
+var util = require('gulp-util');
 
 gulp.task('minify:js', function () {
-    return gulp.src('src/*.js')
+    return gulp.src('src/**/*.js')
+        .pipe(sourcemaps.init())
+        .pipe(concat('analogy.js'))
+        .pipe(gulp.dest('dist/js'))
+        .pipe(rename('analogy.min.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('dist'));
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('default', ['minify:js']);
